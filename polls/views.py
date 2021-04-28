ba0156a5669840fc8fc497f85f2a06bde31889e5
Your Name <you@example.com>
2021-04-28 12:47:57 +0000

    init

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("djangoチュートリアルアプリ")
+    
+def detail(request,question_id):
+    return HttpResponse("詳細画面" % question_id)
+
+
+def results(request,question_id):
+    response="質問結果の表示"
+    return HttpResponse(response % question_id)
+    
+def vote(request,question_id):
+    return HttpResponse("質問の投稿")
 # Create your views here.
